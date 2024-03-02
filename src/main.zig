012393afdb08d1e9c79101872b2e4f3eff7b86de
314lipczuk <3.14lipczuk@gmail.com>
2024-03-03 00:17:38 +0100

    round blink, middle of debugging score count

--- src/main.zig
+++ src/main.zig
@@ -8,15 +8,22 @@ const PlayerChoice = struct { position: bool, symbol: bool };
 
 // game config
 const legal_chars = [_]u8{
-    'a',
-    't',
-    'e',
-    'n',
-    'r',
+    'A',
+    'T',
+    'E',
+    'N',
+    'R',
 };
-const seconds_per_step = 1;
+
+const seconds_per_step = 3;
 const game_steps = 30;
 const X_back = 2;
+
+const amount_of_position_points_to_score = game_steps / 6;
+const chance_for_same_position: f32 = 1 / amount_of_position_points_to_score;
+
+const amount_of_char_points_to_score = game_steps / 6;
+const chance_for_same_char_point: f32 = 1 / amount_of_char_points_to_score;
 
 // graphical config
 const screenWidth = 450;
@@ -35,6 +42,11 @@ var game_states = [1]?Frame{null} ** game_steps;
 var player_choices = [1]?PlayerChoice{null} ** game_steps;
 var start_timestep: f64 = 0;
 
+var score = rl.Vector2.init(0, 0);
+var total_score = rl.Vector2.init(0, 0);
+
+var blink = false;
+
 pub fn main() anyerror!void {
     // Initialization // --------------------------------------------------------------------------------------
     var rand = RandGen.init(0);
@@ -45,8 +57,6 @@ pub fn main() anyerror!void {
     var cursorPosition = rl.Vector2.init(-100, -100);
     var frame = Frame{ .char = 'c', .position = Position.left_down };
     var current_choice = PlayerChoice{ .position = false, .symbol = false };
-
-    var score = rl.Vector2.init(0, 0);
 
     rl.setTargetFPS(30); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -69,15 +79,17 @@ pub fn main() anyerror!void {
                     player_choices[i] = null;
                     game_states[i] = null;
                 }
+                score.x = 0;
+                score.y = 0;
                 is_running = true;
                 start_timestep = rl.getTime();
             }
         }
-        if (!is_running) {}
 
-        handleGameState(&rand, &frame, &current_choice, &score);
+        handleGameState(&rand, &frame, &current_choice);
 
-        // Draw //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(rl.Color.white);
@@ -93,7 +105,7 @@ pub fn main() anyerror!void {
             draw_grid();
             render_frame(&frame);
         } else {
-            draw_stop_screen(&score);
+            draw_stop_screen();
         }
     }
 }
@@ -126,9 +138,13 @@ fn render_frame(f: *Frame) void {
         25,
         rl.Color.light_gray,
     );
+    if (blink) {
+        rl.drawCircle((screenWidth / 2), 50, 5, rl.Color.gold);
+        blink = false;
+    }
 }
 
-fn handleGameState(rand: *RandGen, frame: *Frame, control: *PlayerChoice, score: *rl.Vector2) void {
+fn handleGameState(rand: *RandGen, frame: *Frame, control: *PlayerChoice) void {
     if (!is_running) return;
 
     const t = rl.getTime();
@@ -137,7 +153,7 @@ fn handleGameState(rand: *RandGen, frame: *Frame, control: *PlayerChoice, score:
 
     if (c_step == game_steps) {
         is_running = false;
-        calculate_score(score);
+        calculate_score();
         return;
     }
 
@@ -148,27 +164,33 @@ fn handleGameState(rand: *RandGen, frame: *Frame, control: *PlayerChoice, score:
     if (game_states[c_step] == null) {
         game_states[c_step] = frame.*;
         player_choices[c_step] = control.*;
-        frame.position = std.rand.Random.enumValue(rand.random(), Position);
+        frame.position = if (c_step > 1 and std.rand.Random.float(rand.random(), f32) < chance_for_same_position) game_states[c_step - 2].?.position else std.rand.Random.enumValue(rand.random(), Position);
         frame.char = legal_chars[std.rand.Random.uintLessThan(rand.random(), u8, legal_chars.len)];
+        blink = true;
     }
 
     step = c_step;
 }
 
-fn calculate_score(score: *rl.Vector2) void {
+fn calculate_score() void {
+    std.debug.print("calculate score call\n", .{});
     for (2..game_steps) |i| {
         const true_state = game_states[i - X_back].?;
         const second_true_state = game_states[i].?;
         const player_choice = player_choices[i];
+        std.debug.print("####\nSymbol: {c} : {c} => {?}\n{?} : {?} => {?}\n{?}\n\n\n", .{ true_state.char, game_states[i].?.char, true_state.char == game_states[i].?.char, true_state.position, game_states[i].?.position, true_state.position == game_states[i].?.position, player_choice });
         if (player_choice != null) {
             score.x += if (player_choice.?.position and second_true_state.position == true_state.position) 1 else 0;
             score.y += if (player_choice.?.symbol and second_true_state.char == true_state.char) 1 else 0;
         }
+        total_score.x += if (second_true_state.position == true_state.position) 1 else 0;
+        total_score.y += if (second_true_state.char == true_state.char) 1 else 0;
     }
+    std.debug.print("final score: {?},\ntotal score: {?}", .{ score, total_score });
 }
 
-fn draw_stop_screen(score: *rl.Vector2) void {
+fn draw_stop_screen() void {
     rl.drawText(rl.textFormat("Press s to start", .{}), @intFromFloat(100), @intFromFloat(10), 20, rl.Color.dark_gray);
-    rl.drawText(rl.textFormat("Score position:\t%03d / %03d\nScore symbol:\t%03d / %03d", .{ score.x, @as(u32, game_steps), score.y, @as(u32, game_steps) }), @intFromFloat(30), @intFromFloat(40), 20, rl.Color.dark_gray);
+    rl.drawText(rl.textFormat("Score position:\t%03d / %03d\nScore symbol:\t%03d / %03d", .{ score.x, total_score.x, score.y, total_score.y }), @intFromFloat(30), @intFromFloat(40), 20, rl.Color.dark_gray);
     rl.drawText(rl.textFormat("Config:\n\nMaxIter:\t\t\t%02d\n\nN count:\t\t\t%02d\n\nTicks/s:\t\t\t%02d", .{ @as(u32, game_steps), @as(u32, X_back), @as(u32, seconds_per_step) }), @intFromFloat(10), @intFromFloat(80), 35, rl.Color.dark_gray);
 }
